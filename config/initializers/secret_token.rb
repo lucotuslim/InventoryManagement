ae0e25ea1fde65c4c0027b5d0d464b53307f1c2e
Lim Chee Yong <limcheeyong1978@gmail.com>
2012-07-05 13:46:47 +0800

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InventoryManagement::Application.config.secret_token = '655e362c979ccd326c855c11feae12c64bd5c5e0393b93fbbc5bf5381c0bcbf5362b2cf4f8eb3e7eeb0941b02ebe3856e7e3ef13df71eb666c6added4a6d900b'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+InventoryManagement::Application.routes.draw do
+  root :to => "home#index"
+end
