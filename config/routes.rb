14b3548239bbddd9e30de82c692295016e57ff5d
Lim Chee Yong <limcheeyong1978@gmail.com>
2012-07-08 22:31:16 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 InventoryManagement::Application.routes.draw do
   resources :users 
-
+  resources :sessions, only: [:new,:create,:destroy]
   root :to => "home#index"
-  match '/signup',  to: 'users#new'
+  match '/signup',  to: 'users#new' 
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 end
